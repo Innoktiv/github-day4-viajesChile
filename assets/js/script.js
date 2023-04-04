c2639d9a4b2a595edbbc660cde66ad9d400db5cd
Claudio Dee <claudio.d@innkv.com>
2023-04-04 09:59:59 -0400

    Upload directories

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+// Para despliege automático de imagenes
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Cambia la imagen cada 5 segundos
+}
